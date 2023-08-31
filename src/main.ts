13a715797f6402f2e8c03a80ec942087e2e2ae46
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2023-08-31 13:55:46 +0200

    add code

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,55 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import SpotifyApi from 'spotify-web-api-node';
+import express from 'express';
+import { configDotenv } from 'dotenv';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+configDotenv();
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+const app = express();
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+const spotifyApi = new SpotifyApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'http://localhost:3000/callback',
+});
+
+app.get('/login', (_req, res) => {
+  const scopes = ['user-read-private', 'user-read-email', 'user-library-read'];
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes,'Development mode');
+  res.redirect(authorizeURL);
+});
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+  try {
+    const data = await spotifyApi.authorizationCodeGrant(code);
+    const { access_token, refresh_token } = data.body;
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+    res.redirect('/home');
+  } catch (err) {
+    console.error(err);
+    res.redirect('/login');
+  }
+});
+
+app.get('/home', async (_req, res) => {
+  try {
+
+    const playlists = await spotifyApi.getMySavedTracks()
+
+    res.json(playlists.body.items.map((item) => {
+      return {
+        name: item.track.name,
+        artist: item.track.artists[0].name,
+        album: item.track.album.name,
+      };
+    }));
+  } catch (err) {
+    console.error(err);
+    res.redirect('/login');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});

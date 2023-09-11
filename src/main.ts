6c00db7132743c1cb19eb0b0cab6fd447d16f712
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2023-09-11 18:32:41 +0200

    add additional properties

--- src/main.ts
+++ src/main.ts
@@ -70,26 +70,62 @@ async function createPlaylists(
   }
 }
 
+function songPropertiesMapper(mergedTrack: SpotifyApi.AudioFeaturesObject & Omit<SpotifyApi.TrackObjectFull, 'type'>){
+   return ({
+    name: mergedTrack.name,
+    artists: mergedTrack.artists,
+    album: mergedTrack.album,
+    type: mergedTrack.type,
+    popularity: mergedTrack.popularity,
+    acousticness: mergedTrack.acousticness,
+    energy: mergedTrack.energy,
+    danceability: mergedTrack.danceability,
+    explicit: mergedTrack.explicit,
+    instrumentalness: mergedTrack.instrumentalness,
+    key: mergedTrack.key,
+    liveness: mergedTrack.liveness,
+    loudness: mergedTrack.loudness,
+    mode: mergedTrack.mode,
+    speechiness: mergedTrack.speechiness,
+    tempo: mergedTrack.tempo,
+    time_signature: mergedTrack.time_signature,
+    valence: mergedTrack.valence,
+  })
+}
+
 async function addMySavedTracksToPlaylist(
   playlistsCreated: { body: SpotifyApi.CreatePlaylistResponse }[],
   options: { limit: number; offset: number },
 ): Promise<SpotifyApi.UsersSavedTracksResponse> {
   const { limit, offset } = options;
 
-  console.log('Getting saved tracks', { limit, offset })
-
-  let savedTracks = null;
+  let savedTracks: Response<SpotifyApi.UsersSavedTracksResponse> = null;
+  let tracksWithMood: Response<SpotifyApi.MultipleAudioFeaturesResponse> = null;
   try {
     savedTracks = await spotifyApi.getMySavedTracks({
       limit,
       offset,
     });
+    tracksWithMood = await spotifyApi.getAudioFeaturesForTracks(
+      savedTracks.body.items.map((item) => item.track.id),
+    );
   } catch (err) {
     console.log('Error getting saved tracks', err);
     throw err;
   }
 
+  type MergedTrack = SpotifyApi.AudioFeaturesObject & Omit<SpotifyApi.TrackObjectFull, 'type'>
+
   let aiOrganizedPlaylists = null;
+  const mergedTracksAudioFeaturesWithTracksInfo: (MergedTrack)[] = [];
+
+  for (const savedTrackIndex in savedTracks.body.items) {
+    mergedTracksAudioFeaturesWithTracksInfo.push({
+      ... savedTracks[savedTrackIndex],
+      ...tracksWithMood.body.audio_features[savedTrackIndex],
+    });
+  }
+
   try {
     aiOrganizedPlaylists = await createChatCompletion({
       messages: [
@@ -101,14 +137,7 @@ async function addMySavedTracksToPlaylist(
           role: 'user',
           content: JSON.stringify({
             playlists: playlistsCreated.map((playlist) => playlist.body),
-            songs: savedTracks.body.items.map((item) => {
-              return {
-                name: item.track.name,
-                artist: item.track.artists[0].name,
-                album: item.track.album.name,
-                id: item.track.id,
-              };
-            }),
+            songs: mergedTracksAudioFeaturesWithTracksInfo.map(songPropertiesMapper),
           }),
         },
       ],
@@ -149,11 +178,6 @@ app.post('/playlist', async (req, res) => {
       savedSongsCountInGivenStep > 0 ||
       savedSongsCountInGivenStep === null
     ) {
-      console.log({
-        savedSongsCountInGivenStep,
-        offset,
-        limit: step,
-      });
       const savedSongsInCurrentStep = await addMySavedTracksToPlaylist(
         playlistsCreated,
         {
@@ -164,7 +188,6 @@ app.post('/playlist', async (req, res) => {
 
       savedSongsCountInGivenStep = savedSongsInCurrentStep.items.length;
       offset += step;
-
     }
 
     res.send({ savedCount: offset * step });
@@ -195,3 +218,4 @@ app.use(express.static(`${__dirname}/website`));
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+
